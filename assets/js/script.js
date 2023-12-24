9c500889169f8134c3ba13207e200cad44c7482f
sanchez619 <>
2023-12-24 22:37:16 +0000

    feat: created basic pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log("Hello!");
+//Goal: Get name input of user
+
+//Goal: Make fox call the user by name
+
+//Optional goal: Create multiple text boxes
+
+//Goal: Save associations of users for radicals
+
+//Goal: Save association in array
+
+//Goal: Change radical for question
+
+//Goal: Create radical array
+
+//Goal: Display feedback site
+
+//Goal: Save association of users for kanji
+
+//Goal: Save association in array
+
+//Goal: Change kanji for question
+
+//Goal: Create 10 kanji arrays
+
+//Goal: Display feedback site
