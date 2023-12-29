eebd7c3c68a52d6cee398b1efdeae452d6aeb52d
sanchez619 <>
2023-12-29 16:20:16 +0000

    chore: add variables for visible quiz features

--- assets/js/rQuiz.js
+++ assets/js/rQuiz.js
@@ -1,3 +1,8 @@
 /*Global Constants - mainly question text*/
 const choice = document.getElementsByClassName("choiceContent")
 
+/*Global Variables - general quiz features*/
+let currentQuestion = {};
+let acceptedAnswers = true;
+let score = 0;
+let questionCount = 0;
