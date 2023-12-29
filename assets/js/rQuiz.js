60887681b1cf561c6f61523b63d1206ba5faf861
sanchez619 <>
2023-12-29 22:38:24 +0000

    feat: add correct/ / incorrect class - background does not change

--- assets/js/rQuiz.js
+++ assets/js/rQuiz.js
@@ -4,7 +4,7 @@ const quizLength = 3;
 
 /*Global Variables - general quiz features*/
 let currentQuestion = {};
-let acceptedAnswers = true;
+let acceptedAnswers = false;
 let score = 0;
 let questionCount = 0;
 let questionsLeft = [];
@@ -12,35 +12,34 @@ let questionsLeft = [];
 /*Array of Radicals and their properties */
 /*Array of possible questions */
 let questions = [{
-        choice1: "user input",
-        choice2: "random",
-        choice3: "random",
-        choice4: "random",
-        answer: 1,
-    },
+    choice1: "user input",
+    choice2: "random",
+    choice3: "random",
+    choice4: "random",
+    answer: 1,
+},
 {
-    
+
     choice1: "random",
     choice2: "random",
     choice3: "user input",
     choice4: "random",
-        answer: 3,
-    },
+    answer: 3,
+},
 {
     choice1: "random",
     choice2: "random",
     choice3: "random",
     choice4: "user input",
     answer: 4,
-    },
+},
 ];
 
 /*function for starting game*/
 launchGame = () => {
     questionCount = 0;
     score = 0;
-    questionsLeft = [... questions];
-    console.log(questionsLeft);
+    questionsLeft = [...questions];
     exchangeQuestion();
 };
 
@@ -49,7 +48,7 @@ exchangeQuestion = () => {
     questionCount++;
     let questionIndex = Math.floor(Math.random() * questionsLeft.length);
     currentQuestion = questionsLeft[questionIndex];
-    choices.forEach((choice) => {
+    choices.forEach(choice => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
     });
@@ -57,16 +56,28 @@ exchangeQuestion = () => {
     acceptedAnswers = true;
 };
 
-/*Event Listener for selected choice*/
-choices.forEach((choice) => {
+/* */
+choices.forEach(choice => {
+    /*Event Listener for selected choice*/
     choice.addEventListener("click", e => {
         if (!acceptedAnswers) return;
         acceptedAnswers = false;
         let selectedContent = e.target;
-        let selectedNumber = selectedContent.dataset['number'];
-        exchangeQuestion();
+        let selectedAnswer = selectedContent.dataset["number"];
+
+        let applyClass = "incorrect";
+        if (selectedAnswer == currentQuestion.answer) {
+            applyClass = "correct";
+        };
+        console.log(applyClass)
+        selectedContent.parentElement.classList.add(applyClass);
+
+        setTimeout(() => {
+            selectedContent.parentElement.classList.remove(applyClass);
+            exchangeQuestion();
+        }, 1000);
     });
 });
 
 /*Starts Game*/
-launchGame()
+launchGame();
