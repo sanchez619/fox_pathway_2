3c63fbefc0efd0b70111c9a06f0e87f7a699a8eb
sanchez619 <>
2023-12-29 16:30:21 +0000

    feat: add arrays for future question and radical objects

--- assets/js/rQuiz.js
+++ assets/js/rQuiz.js
@@ -6,3 +6,43 @@ let currentQuestion = {};
 let acceptedAnswers = true;
 let score = 0;
 let questionCount = 0;
+let questionsLeft = [];
+
+let radicals = [
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+]
+
+let questions = [{
+        choice1: "user input",
+        choice2: "random",
+        choice3: "random",
+        choice4: "random",
+        answer: 1,
+    },
+{
+    
+    choice1: "random",
+    choice2: "random",
+    choice3: "user input",
+    choice4: "random",
+        answer: 3,
+    },
+{
+    choice1: "random",
+    choice2: "random",
+    choice3: "random",
+    choice4: "user input",
+    answer: 4,
+    },
+];
