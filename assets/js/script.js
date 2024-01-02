517c002f7cd3fc0bdf09cd6b4c6289037132572e
sanchez619 <>
2024-01-02 15:22:50 +0000

    chore: add event + listener for changing the 1st div content - bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 /*index.html Global Constants */
 const welcomeArea = document.getElementById('welcomeArea');
-const nameForm = document.getElementById('nameField');
-
-let username
-
-
-
+const nameRequest = document.getElementById('nameRequest');
 
 //Goal: Get name input of user
+let username = document.getElementById('nameField')
 
+nameForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    username = event.target[0].value;
+    nameRequest.classList.add('hide');
+    welcomeArea.classList.remove('hide');
+    document.document.getElementById('welcomeArea').innerHTML = "Welcome, " + username + "!" + "<br>" + "Click on the middle button to get to the Radical Quiz!" + "<br>" + "Click on the middle button to get to the Kanji Quiz!"; 
+});
 //Goal: Make fox call the user by name
+
+
 
 //Optional goal: Create multiple text boxes
 
