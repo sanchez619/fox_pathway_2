f17e8b03ce4ff48a624b56328c0f9265051325a4
sanchez619 <>
2024-01-03 21:15:08 +0000

    fix: change major styling issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,30 +10,6 @@ nameRequest.addEventListener('submit', (event) => {
     username = event.target[0].value;
     nameRequest.classList.add('hide');
     welcomeArea.classList.remove('hide');
-    document.getElementById('welcomeArea').innerHTML = "Welcome, " + username + "!" + "<br>" + "Click on the middle button to get to the Radical Quiz!" + "<br>" + "Click on the middle button to get to the Kanji Quiz!"; 
+    document.getElementById('welcomeArea').innerHTML = "Welcome, " + username + "!" + "<br>" + "Click on the lower box to get to the Radical Quiz!"; 
 });
 //Goal: Make fox call the user by name
-
-
-
-//Optional goal: Create multiple text boxes
-
-//Goal: Save associations of users for radicals
-
-//Goal: Save association in array
-
-//Goal: Change radical for question
-
-//Goal: Create radical array
-
-//Goal: Display feedback site
-
-//Goal: Save association of users for kanji
-
-//Goal: Save association in array
-
-//Goal: Change kanji for question
-
-//Goal: Create 10 kanji arrays
-
-//Goal: Display feedback site
