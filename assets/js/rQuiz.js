29f2dafb1c1cf4228c1e172e94c134380962e636
Sebastian Bucher <Sebasti.b@web.de>
2024-03-01 17:43:48 +0000

    fix: erase redundant double code

--- assets/js/rQuiz.js
+++ assets/js/rQuiz.js
@@ -18,116 +18,113 @@ let questions = [];
 /*fetch function to grab questions from json file */
 
 fetch('assets/js/questions.json')
-    .then(function(res) {
-        if (!res.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return res.json();
-    })
-    .then(function(loadedQuestions) {
-        questions = loadedQuestions;
-        shuffleArray(questions);
-        questions = questions.slice(0, 10);
-        launchGame();
-    })
-    .catch(function(err) {
-        console.error('Error fetching questions:', err);
-    });
+  .then(function (res) {
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return res.json();
+  })
+  .then(function (loadedQuestions) {
+    questions = loadedQuestions;
+    shuffleArray(questions);
+    questions = questions.slice(0, 10);
+    launchGame();
+  })
+  .catch(function (err) {
+    console.error('Error fetching questions:', err);
+  });
 
 /*Listener grabs user input and changes it to first answer in question */
-associationForm.addEventListener('submit', function(event) {
-    event.preventDefault();
+associationForm.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-    const userInput = event.target[0].value.toLowerCase();
-    if (userInput !== '') {
+  const userInput = event.target[0].value.toLowerCase();
+  if (userInput !== '') {
     questions[questionCount].choice1 = userInput;
     questions[questionCount].answer = userInput;
 
+    console.log(questions[questionCount]);
+    console.log(questions[questionCount].choice1);
+    console.log(questions[questionCount].answer);
+
     event.target.reset();
     questionCount++;
-    } else {
-        alert('Input empty. Please add a sufficient association.');
-    }
-    
-    /*changes visible items from form to quiz*/
-    if (questionCount == quizLength) {
-        questionArea.classList.add('hide');
-        quizArea.classList.remove('hide');
-        questionCount = 0;
-        questionsLeft = questions.slice();
-        radicalPicture.style.backgroundImage = "url('" + questions[questionCount].picture + "')";
-        exchangeQuestion();
-        return;
-    }
+  } else {
+    alert('Input empty. Please add a sufficient association.');
+  }
 
-    radicalPicture.style.backgroundImage = "url('" + questions[questionCount].picture + "')";
-    startAssociations();
+  /*changes visible items from form to quiz*/
+  if (questionCount == quizLength) {
+    questionArea.classList.add('hide');
+    quizArea.classList.remove('hide');
+    questionCount = 0;
+    questionsLeft = questions.slice();
+    radicalPicture.style.backgroundImage =
+      "url('" + questions[questionCount].picture + "')";
+    exchangeQuestion();
+    return;
+  }
+
+  radicalPicture.style.backgroundImage =
+    "url('" + questions[questionCount].picture + "')";
+  startAssociations();
 });
 
 /*function for starting game - at associations*/
 function launchGame() {
-    questionCount = 0;
-    score = 0;
-    startAssociations();
+  questionCount = 0;
+  score = 0;
+  startAssociations();
 }
 
 function startAssociations() {
-    for (let i = 0; i < question.length; i++) {
-      question[i].innerHTML = questions[questionCount].question;
-    }
-    radicalPicture.style.backgroundImage =
-      "url('" + questions[questionCount].picture + "')";
+  for (let i = 0; i < question.length; i++) {
+    question[i].innerHTML = questions[questionCount].question;
+  }
+  radicalPicture.style.backgroundImage =
+    "url('" + questions[questionCount].picture + "')";
 }
-  
-  /*function for replacing questions*/
-function exchangeQuestion() {
-    questionCount++;
-  
-    if (questionsLeft.length === 0 || questionCount > quizLength) {
-      localStorage.setItem('endScore', score);
-      /*automatically connect to result page*/
-      return window.location.assign('result.html');
-    }
-    radicalPicture.style.backgroundImage =
-      "url('" + questions[questionCount - 1].picture + "')";
-    let questionIndex = Math.floor(Math.random() * questionsLeft.length);
-    currentQuestion = questionsLeft[questionIndex];
-    question[1].innerText = currentQuestion.question;
-    let choicesArray = [
-      currentQuestion.choice1,
-      currentQuestion.choice2,
-      currentQuestion.choice3,
-      currentQuestion.choice4,
-    ];
-    shuffleArray(choicesArray);
-  
-    for (let i = 0; i < choicesArray.length; i++) {
-      currentQuestion['choice' + (i + 1)] = choicesArray[i];
-    }
-  
-    /* Display the question and shuffled choices */
-    question.innerText = currentQuestion.question;
-    choices.forEach(function (choice) {
-      const choiceContent = choice.querySelector('.choiceContent');
-      const number = choiceContent.dataset.number;
-      choiceContent.innerText = currentQuestion['choice' + number];
-    });
-  
-    questionsLeft.splice(questionIndex, 1);
-    acceptedAnswers = true;
-}
-  
-/* Display the question and shuffled choices */
-question.innerText = currentQuestion.question;
-choices.forEach(function (choice) {
-  const choiceContent = choice.querySelector('.choiceContent');
-  const number = choiceContent.dataset.number;
-  choiceContent.innerText = currentQuestion['choice' + number];
-});
-questionsLeft.splice(questionIndex, 1);
-acceptedAnswers = true;
 
-/*Add correct and incorrect classes to user choice */
+/*function for replacing questions*/
+function exchangeQuestion() {
+  questionCount++;
+
+  if (questionsLeft.length === 0 || questionCount > quizLength) {
+    localStorage.setItem('endScore', score);
+    /*automatically connect to result page*/
+    return window.location.assign('result.html');
+  }
+  radicalPicture.style.backgroundImage =
+    "url('" + questions[questionCount - 1].picture + "')";
+  let questionIndex = Math.floor(Math.random() * questionsLeft.length);
+  currentQuestion = questionsLeft[questionIndex];
+  question[1].innerText = currentQuestion.question;
+  let choicesArray = [
+    currentQuestion.choice1,
+    currentQuestion.choice2,
+    currentQuestion.choice3,
+    currentQuestion.choice4,
+  ];
+  shuffleArray(choicesArray);
+
+  for (let i = 0; i < choicesArray.length; i++) {
+    currentQuestion['choice' + (i + 1)] = choicesArray[i];
+  }
+
+  /* Display the question and shuffled choices */
+  question.innerText = currentQuestion.question;
+  choices.forEach(function (choice) {
+    const choiceContent = choice.querySelector('.choiceContent');
+    const number = choiceContent.dataset.number;
+    choiceContent.innerText = currentQuestion['choice' + number];
+  });
+
+  questionsLeft.splice(questionIndex, 1);
+  acceptedAnswers = true;
+}
+
+// Assign the shuffled choices back to the currentQuestion object
+
 /*Add correct and incorrect classes to user choice */
 choices.forEach(function (choiceBox) {
   choiceBox.addEventListener('click', function (e) {
